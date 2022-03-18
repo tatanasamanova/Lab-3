87f136962194de669c82ccc0f01ad80cf836c9af
tatana <tatanasamanova38@gmail.com>
2022-03-18 18:48:32 +0300

    Commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main (String[]args) {
+        for (int a = 1; a <= 10; a++) {
+            for (int b = 1; b<= 10; b++) {
+                System.out.println(a+ " * " + b + " = " + a * b);
+            }
+            System.out.println();
+        }
+    }
+}
